28ed014ac96c934442bbf410966cdfa77e3c0875
tarekul42 <tarekulrifat205@gmail.com>
2023-07-11 16:15:24 +0600

    header added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Nav/Header'
+import logo from '/Logo.svg'
+
+function App() {
+
+  return (
+    <>
+    <Header></Header>
+
+    </>
+  )
+}
+
+export default App
