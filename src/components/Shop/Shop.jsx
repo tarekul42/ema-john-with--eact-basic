70339f3d0b8d163018d5a19c456c9f7e143363bb
tarekul42 <tarekulrifat205@gmail.com>
2023-07-14 10:28:40 +0600

    added cart component and calculation

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -31,8 +32,7 @@ const Shop = () => {
             </div>
 
             <div className="cart-container">
-                <h1>Cart</h1>
-                <p>Selected Item: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
             
         </div>
